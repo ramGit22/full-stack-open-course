7e4a5919c092c21196ce075a460a3aeda1ad6e71
ramGit22 <karki_ram@hotmail.com>
2023-08-24 13:50:07 +0300

    task: map array of objects

--- part2/course-info/src/components/Course.js
+++ part2/course-info/src/components/Course.js
@@ -2,19 +2,27 @@ import Content from "./Content";
 import Header from "./Header";
 import Total from "./Total";
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
   const calculateTotal = (parts) => {
     return parts.reduce(
       (accumulator, currentValue) => accumulator + currentValue.exercises,
       0
     );
   };
+
   return (
-    <div>
-      <Header title={course.name} />
-      <Content parts={course.parts} />
-      <Total total={calculateTotal(course.parts)} />
-    </div>
+    <>
+      <div>
+        <h1>Web development curriculum</h1>
+        {courses.map((course) => (
+          <div key={course.id}>
+            <Header title={course.name} />
+            <Content parts={course.parts} />
+            <Total total={calculateTotal(course.parts)} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- part2/course-info/src/components/Header.js
+++ part2/course-info/src/components/Header.js
@@ -1,5 +1,5 @@
 const Header = ({ title }) => {
-  return <h1>{title}</h1>;
+  return <h2>{title}</h2>;
 };
 
 export default Header;
