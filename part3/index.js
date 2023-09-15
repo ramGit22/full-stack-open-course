231c5aa77a2ed87deb8bf5e6c710f0c0ebfcb7f8
ramGit22 <karki_ram@hotmail.com>
2023-09-15 10:56:56 +0300

    task: 3.13

--- part3/index.js
+++ part3/index.js
@@ -2,34 +2,13 @@ const morgan = require("morgan");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const Person = require("./models/person");
+
 const app = express();
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.static("build"));
-
-const persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 morgan.token("req-body", (req, res) => {
   return JSON.stringify(req.body);
@@ -37,8 +16,10 @@ morgan.token("req-body", (req, res) => {
 
 app.use(morgan(":method :url :status :req-body"));
 
-app.get("/api/persons", (req, res) => {
-  res.send(persons);
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (req, res) => {
