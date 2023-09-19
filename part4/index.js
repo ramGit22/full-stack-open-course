cfde1e08cad4205b8100ea07391659bbc7792f90
ramGit22 <karki_ram@hotmail.com>
2023-09-19 15:59:41 +0300

    task: 4.2

--- part4/index.js
+++ part4/index.js
@@ -1,20 +1,11 @@
+const config = require("./utils/config");
+const blogRouter = require("./controllers/blogs");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
-
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = process.env.MONGODB_URI;
+const mongoUrl = config.MONGODB_URI;
 mongoose
   .connect(mongoUrl)
   .then(() => {
@@ -23,22 +14,10 @@ mongoose
   .catch((error) => {
     console.log("error connecting to MongoDb:", error.message);
   });
+
+app.use("/api/blogs", blogRouter);
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
 
 const PORT = 3003;
 app.listen(PORT, () => {
