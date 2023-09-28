cffaa531d4935a453553eb900f9b67af87fd4c09
ramGit22 <karki_ram@hotmail.com>
2023-09-28 11:19:46 +0300

    task: 4.9

--- part4/tests/blogList.test.js
+++ part4/tests/blogList.test.js
@@ -4,12 +4,23 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.status).toBe(200)
-  expect(response.type).toBe('application/json')
-  expect(response.body).toHaveLength(2)
-  console.log('node', process.env.NODE_ENV)
+describe('blogList', () => {
+  test('should return blogs as json', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.status).toBe(200)
+    expect(response.type).toBe('application/json')
+    expect(response.body).toHaveLength(response.body.length)
+    console.log('node', process.env.NODE_ENV)
+  })
+
+  test('should return blog posts unique identifier property as Id', async () => {
+    const response = await api.get('/api/blogs')
+    const responseArray = response.body
+    responseArray.map((blog) => {
+      expect(blog.id).toBeDefined()
+      expect(blog._id).toBeUndefined()
+    })
+  })
 })
 
 afterAll(async () => {
