3379c2de0e092525306c71ec1d9f2aa73c8bfed2
ramGit22 <karki_ram@hotmail.com>
2023-10-10 16:51:13 +0300

    task: 4.19

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,8 @@
+const jwt = require('jsonwebtoken')
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+require('dotenv').config()
 
 blogRouter.get('/', async (request, response) => {
   try {
@@ -11,27 +13,46 @@ blogRouter.get('/', async (request, response) => {
   }
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findOne()
-  if (!user) {
-    return response.status(400).send({ error: 'No user found in the database' })
+  const token = getTokenFrom(request)
+  if (!token) {
+    return response.status(401).json({ error: 'token missing' })
   }
-  if (!body.title || !body.url) {
-    return response.status(400).send({ error: 'title or url missing' })
-  }
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0,
-    user: user.id,
-  })
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    if (!user) {
+      return response
+        .status(400)
+        .send({ error: 'No user found in the database' })
+    }
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+      user: user.id,
+    })
 
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
-  response.json(savedBlog)
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.json(savedBlog)
+  } catch (error) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
 })
 
 blogRouter.put('/:id', async (req, res) => {
