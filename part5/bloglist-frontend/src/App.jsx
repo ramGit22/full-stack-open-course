47b0bee674a974ffad5bf50ecf9fb43655a1cd4d
ramGit22 <karki_ram@hotmail.com>
2023-11-09 16:34:04 +0200

    logout functionality

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -8,13 +8,12 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-  const [refresh, setRefreshBlog] = useState(false)
 
   useEffect(() => {
     if (user) {
       blogService.getAll().then((blogs) => setBlogs(blogs))
     }
-  }, [user, refresh])
+  }, [user])
 
   useEffect(() => {
     const storedUser = window.localStorage.getItem('loggedUser')
@@ -35,13 +34,17 @@ const App = () => {
       })
       window.localStorage.setItem('loggedUser', JSON.stringify(user))
       blogService.setToken(user.token)
-      setRefreshBlog(true)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (error) {
       console.log(error)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
   }
 
   if (user === null) {
@@ -80,6 +83,7 @@ const App = () => {
       <div>
         <h2>Blogs</h2>
         <p>{user.username} logged in</p>
+        <button onClick={handleLogout}>Log Out</button>
         {blogs.map((blog) => (
           <Blog key={blog.id} blog={blog} />
         ))}
