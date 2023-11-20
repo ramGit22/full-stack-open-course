56d4a29547e77463f0be34663e97034e860ce3f5
ramGit22 <karki_ram@hotmail.com>
2023-11-20 14:37:51 +0200

    task 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,3 +1,36 @@
-const Blog = ({ blog }) => <div>{blog.title}</div>
+import { useState } from 'react'
 
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  const [showView, setshowView] = useState(false)
+  const handleShow = () => {
+    setshowView(!showView)
+    setbuttonName('hide')
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}{' '}
+      <button onClick={handleShow}>{showView ? 'hide' : 'view'}</button>
+      <br />
+      {showView ? (
+        <div>
+          {' '}
+          {blog.url} <br />
+          {blog.likes} <button>like</button>
+          <br />
+          {blog.author}
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  )
+}
 export default Blog
