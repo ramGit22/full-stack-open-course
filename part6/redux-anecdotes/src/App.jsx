360676f7fa35fc5fefb5ce77ae33ab5ee59f7130
ramGit22 <karki_ram@hotmail.com>
2024-01-11 15:14:56 +0200

    task 6.14

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { useDispatch } from 'react-redux'
+import { initializeAncedotes } from './reducers/anecdoteSlice'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeAncedotes())
+  }, [dispatch])
   return (
     <>
       <Notification />
--- part6/redux-anecdotes/src/reducers/anecdoteSlice.jsx
+++ part6/redux-anecdotes/src/reducers/anecdoteSlice.jsx
@@ -1,26 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
-
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-]
+import anecdoteService from '../services/anecdotes'
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  }
-}
-
 const initialState = {
-  anecdotes: anecdotesAtStart.map(asObject),
+  anecdotes: [],
+
   selected: '',
   notification: '',
 }
@@ -56,6 +41,9 @@ const anecdotesSlice = createSlice({
     clearNotification: (state) => {
       state.notification = ''
     },
+    setAnecdotes(state, action) {
+      state.anecdotes = Array.isArray(action.payload) ? action.payload : []
+    },
   },
 })
 
@@ -65,5 +53,14 @@ export const {
   filterAnecdotes,
   setNotification,
   clearNotification,
+  setAnecdotes,
 } = anecdotesSlice.actions
+
+export const initializeAncedotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
 export default anecdotesSlice.reducer
